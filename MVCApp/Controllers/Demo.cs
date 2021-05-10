bfa487584630e9f423f6ed37e373d3bb4b6a124c
sara20202 <zhurisara@outlook.com>
2021-05-10 16:14:10 +0200

    some init msg

---
+++ MVCApp/Controllers/Demo.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApp.Controllers
+{
+    public class Demo : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MoreInfo()
+        {
+            return View();
+        }
+    }
+}
